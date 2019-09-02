9b4565ca74e8748e3c0cd1d3af3f5bbd499aa225
MSI\user <semmyking@gmail.com>
2019-09-02 14:45:36 +0200

    PlayerMovement
    made a script for player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1.5f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            MoveLeft();
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            MoveRight();
+        }
+    }
+
+    private void MoveLeft()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+    private void MoveRight()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+}
