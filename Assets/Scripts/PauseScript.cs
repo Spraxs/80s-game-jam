8c9176eabfe9722603e82ad6b2aea859228ddc7c
MSI\user <semmyking@gmail.com>
2019-09-02 15:01:00 +0200

    added Pause script
    added pause script and test objects to the in game test scene

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject PauseUI;
+    public bool paused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            togglepause();
+        }
+        if (paused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        if (!paused)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void togglepause()
+    {
+        paused = !paused;
+    }
+}
